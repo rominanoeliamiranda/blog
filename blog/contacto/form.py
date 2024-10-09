8a6d374c6a2a6660b649b68f1bf10ddac3f0252d
romina <rominanoeliamiranda@gmail.com>
2024-10-09 20:03:06 -0300

    Dejar de rastrear todos los archivos

--- blog/contacto/form.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import MensajeContacto  
-
-class ContactoForm(forms.ModelForm): 
-    class Meta:
-        model = MensajeContacto 
-        fields = ['nombre', 'email', 'mensaje']  
